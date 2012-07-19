ef8483866a54cc8db9b990f650b97ebc8d3d8408
Adam Martin" <adam.madram@gmail.com>
2012-07-19 10:39:43 -0500

    Converted enginecore to a static lib. This requires a native app project be built and linked against all the appropriate libs.
    For an example see the win32 CMakeLists.txt and main.cpp.

---
+++ src/windows/main.cpp
@@ -0,0 +1,30 @@
+#include <windows.h>
+#include "../windows/win32.h"
+#include "../enginecore/EngineCore.h"
+#include "../sharedbase/EventLogger.h"
+
+int WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in LPSTR lpCmdLine, __in int nShowCmd ) {
+	win32::Make();
+	OSInterfaceSPTR operating_system(OSInterface::GetOSPointer());
+
+	std::string bin_dir(operating_system->GetPath(SYSTEM_DIRS::EXECUTABLE));
+
+	// Load the event logger, logging to the config log location if running under debug or if the 
+	EventLogger* elog = EventLogger::GetEventLogger(); // *TODO: Must fix the mem leak on exit
+	EventLogger::module = "Main";
+	elog->SetLogFile(bin_dir + "/" + NLS_ENGINE_DEFAULT_LOG_FILE);
+	LOG(LOG_PRIORITY::FLOW, "Log file created!");
+
+	EngineCore engine(elog, bin_dir);
+	if (!engine.StartUp()) {
+		LOG(LOG_PRIORITY::FLOW, "Engine startup failed.");
+	}
+
+	while(engine.IsRunning()) {
+		operating_system->RouteMessages();
+		engine.Update();
+	}
+
+	engine.Shutdown();
+	return 0;
+}
